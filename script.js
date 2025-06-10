a1829c9502495dbad39abe41e5e8f037011609c8
erw004 <156253931+erw004@users.noreply.github.com>
2025-06-09 19:21:00 -0700

    .

---
+++ script.js
@@ -0,0 +1,36 @@
+// Written by mlazze@globocorp.com - email me at mlazze1337dev@gmail.com for freelance work!
+let messages = [];
+let count = 0;
+
+function send() {
+    let input = document.getElementById('input');
+    let prompt = input.value;
+
+    if (prompt == '') {
+        alert('Type something!');
+        return;
+    }
+
+    count++;
+    messages.push(prompt);
+
+    let chat = document.getElementById('chat');
+    chat.innerHTML += '<div class="message user">You: ' + prompt + '</div>';
+    input.value = '';
+
+    // TODO: Simulate the chat for now
+    // replace with a to askGPTGenie(prompt)
+
+    setTimeout(function() {
+        let replies = ["Cool!", "Awesome!", "Noice", "Killer idea", "Lowkey fire", "Fire", "Goated idea", "I hear you...", "Sounds like a plan"];
+        let reply = replies[Math.floor(Math.random() * replies.length)];
+        chat.innerHTML += '<div class="message bot">Bot: ' + reply + '</div>';
+        chat.scrollTop = chat.scrollHeight;
+    }, 500);
+    // End Simulate
+
+}
+
+const appVersion = "1.0";
+const appName = "GloboCorp's GPT Genie";
+console.log(appName + " " + appVersion + " loaded @ " + new Date());
